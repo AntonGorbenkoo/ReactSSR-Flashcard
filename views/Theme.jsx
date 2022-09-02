838a15a171db56c0f639923ce02ba1ba32391018
Andrii Domanskyi <andromansky@gmail.com>
2022-09-02 10:51:50 +0300

    Skeleton

--- views/Theme.jsx
+++ views/Theme.jsx
@@ -1,15 +1,16 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-function Theme({theme}){
-    return(
-        <div>
-           <form>
-            <p>{theme.title}</p>
-            <img src={theme.cover} alt={theme.id} />
-            <button>Выбрать тему</button>
-           </form>
-        </div>
-    )
-};
-module.exports = Theme
+function Theme({ theme }) {
+  return (
+    <div>
+        <a href={`/question/${theme.id}/1`}>PUSH ME {theme.id}</a>
+      {/* <form method="get" action={`/question/${theme.id}/1`}>
+        <p>{theme.title}</p>
+        <img src={theme.cover} alt={theme.id} />
+        <button>Выбрать тему</button>
+      </form> */}
+    </div>
+  );
+}
+module.exports = Theme;
